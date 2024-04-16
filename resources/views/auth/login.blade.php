8205d8e04076d024cae90abab6048487fbed571a
Herman Gutierrez <hermangutierrez@gmail.com>
2024-04-16 09:48:04 +0200

    Modificar el registro y login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,12 +11,12 @@
                 {{ $value }}
             </div>
         @endsession
-
+        
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
             <div>
-                <x-label for="email" value="{{ __('Email') }}" />
+                <x-label for="email" value="{{ __('Email') }}"  />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             </div>
 
@@ -32,7 +32,7 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-between mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,6 +7,8 @@
         <x-validation-errors class="mb-4" />
 
         <form method="POST" action="{{ route('register') }}">
+            <h2 class="font-bold mt-2 mb-4 text-center text-2xl text-gray-500 dark:text-white">Regístrate en OpenWebCongress</h2>
+
             @csrf
 
             <div>
@@ -46,7 +48,8 @@
                 </div>
             @endif
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-between mt-4">
+                               
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
